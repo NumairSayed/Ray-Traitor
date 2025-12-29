6fa3b3b8e81c6c761380896119e4107985354a5a
numairsayed <sayednumair2019@gmail.com>
2025-12-29 17:41:24 +0530

    :bricks:: Setup for output buffer and terminal window for rendering tested with the help of colour gradients

---
+++ CApp.cpp
@@ -0,0 +1,88 @@
+#include "CApp.h"
+
+// Constructor
+CApp::CApp()
+{
+
+isRunning = true;
+pWindow = NULL;
+pRenderer = NULL;
+
+};  
+
+bool CApp::OnInit(){
+    if (SDL_Init(SDL_INIT_EVERYTHING)<0){
+        return false;
+    }
+    pWindow = SDL_CreateWindow("qbraytracer window --Woww ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN);
+    if (pWindow != NULL){
+        pRenderer = SDL_CreateRenderer(pWindow, -1, 0);
+
+        // Initialise the qbImage instance.
+		m_image.Initialize(1920, 1080, pRenderer);
+		
+		// Create some colour variations.
+		for (int x=0; x<1920; ++x)
+		{
+			for (int y=0; y<1080; ++y)
+			{
+				double red = (static_cast<double>(x)/1920.0) * 255.0;
+				double green = (static_cast<double>(y)/1080.0) * 255.0;
+				m_image.SetPixel(x, y, red, green, 0.0);
+			}
+		}
+    }
+    else{
+        return false;           
+    }
+    return true;
+}
+
+///////////////////////////////////////////////
+int CApp::OnExecute(){
+    SDL_Event event;
+    if (OnInit() == false){
+        return -1;
+    }
+    while(isRunning){
+        while(SDL_PollEvent(&event) != 0){
+            OnEvent(&event);
+        }
+        OnLoop();
+        OnRender();
+    }
+    OnExit();
+	return 0;
+
+}
+
+void CApp::OnEvent(SDL_Event *event){
+    if (event->type == SDL_QUIT){
+        isRunning = false;
+    }
+
+}
+//////////////////////////////////////////////
+
+void CApp::OnLoop(){
+
+}
+
+void CApp::OnRender(){
+    //set the background colour to white
+    SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
+    SDL_RenderClear(pRenderer);
+    
+    // Display the image.
+	m_image.Display();
+
+    // showing results
+    SDL_RenderPresent(pRenderer);
+}
+void CApp::OnExit(){
+    //graceful tidiness on close
+    SDL_DestroyRenderer(pRenderer);
+    SDL_DestroyWindow(pWindow);
+    pWindow = NULL;
+    SDL_Quit();
+}
